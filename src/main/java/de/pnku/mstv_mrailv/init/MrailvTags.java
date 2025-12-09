35068527a3d1dee28c0f9758ac9443c4e485f00d
pnku <159317511+pnk2u@users.noreply.github.com>
2025-12-09 04:30:56 +0100

    Fix powered/activator rails sharing power (1.0.6|2)
    - Implements vanilla behavior of only sharing power between the same type applied to all variants via tags

--- src/main/java/de/pnku/mstv_mrailv/init/MrailvTags.java
+++ src/main/java/de/pnku/mstv_mrailv/init/MrailvTags.java
@@ -8,6 +8,6 @@ import net.minecraft.world.level.block.Block;
 
 public class MrailvTags {
         public static final TagKey<Block> ALL_POWERED_RAIL = TagKey.create(Registries.BLOCK, MoreRailVariants.asId("all_powered_rail"));
-        public static final TagKey<Block> POWERED_RAIL_VARIANTS = TagKey.create(Registries.BLOCK, MoreRailVariants.asId("powered_rail_variants"));
+        public static final TagKey<Block> ALL_ACTIVATOR_RAIL = TagKey.create(Registries.BLOCK, MoreRailVariants.asId("all_activator_rail"));
         public static final TagKey<Item> RAIL_REDSTONE_TORCHES = TagKey.create(Registries.ITEM, MoreRailVariants.asId("rail_redstone_torches"));
 }
