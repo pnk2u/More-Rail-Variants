9d7dec235e4de1180fbe74dce3c65446b11d37e1
pnku <pnku@pnku.de>
2026-03-09 00:04:50 +0100

    Fix Activator Rail minecart behavior (1.0.7-d:c2)

--- src/main/java/de/pnku/mstv_mrailv/mixin/AbstractMinecartMixin.java
+++ src/main/java/de/pnku/mstv_mrailv/mixin/AbstractMinecartMixin.java
@@ -1,19 +1,31 @@
 package de.pnku.mstv_mrailv.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
+import static de.pnku.mstv_mrailv.init.MrailvTags.ALL_ACTIVATOR_RAIL;
 import static de.pnku.mstv_mrailv.init.MrailvTags.ALL_POWERED_RAIL;
 
 @Mixin(AbstractMinecart.class)
 public abstract class AbstractMinecartMixin {
 
-    @Redirect(method = "moveAlongTrack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 0))
-    protected boolean redirectedMoveAlongTrack(BlockState blockState, Block block) {
-        return blockState.is(ALL_POWERED_RAIL);
+    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z"))
+    protected boolean wrappedTickAtStateIs(BlockState blockState, Block block, Operation<Boolean> original) {
+        if (block.defaultBlockState().getBlock() == Blocks.ACTIVATOR_RAIL) {
+            return blockState.is(ALL_ACTIVATOR_RAIL);
+        } return original.call(blockState, block);
+    }
+
+    @WrapOperation(method = "moveAlongTrack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z"))
+    protected boolean wrappedMoveAlongTrackAtStateIs(BlockState blockState, Block block, Operation<Boolean> original) {
+        if (block.defaultBlockState().getBlock() == Blocks.POWERED_RAIL) {
+            return blockState.is(ALL_POWERED_RAIL);
+        } return  original.call(blockState, block);
     }
 }
